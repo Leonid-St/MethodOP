40081c65b5b4aac382fa1e9f5a780bcd7f40a383
Leonid Stepanov <leonid.stepanov@inrecolan.com>
2021-10-09 23:19:43 +0300

    Добавьте файлы проекта.

---
+++ MethodOP/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace MethodOP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(282, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderWidth = 3;
+            series1.Name = "y=x^2+8e^(0.55x)";
+            series2.BorderWidth = 10;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series2.LabelForeColor = System.Drawing.Color.Maroon;
+            series2.Legend = "Legend1";
+            series2.Name = "x:=-1.16;y:=5.59";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(904, 517);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(306, 91);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.label1.Location = new System.Drawing.Point(23, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X:=";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.label2.Location = new System.Drawing.Point(23, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y:=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.label3.Location = new System.Drawing.Point(69, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 24);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F);
+            this.label4.Location = new System.Drawing.Point(69, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 24);
+            this.label4.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 591);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MethodOP/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace MethodOP
+{
+
+    public partial class Form1 : Form
+    {
+		
+		static double f(double x)
+		{
+			return x * x + 8 * Math.Pow(2.7, 0.55 * x);
+		}
+		/*static  double  D(double a, double b, double E)
+		{
+			string path = Path.GetTempFileName();
+			FileInfo fi1 = new FileInfo(path);
+			double x1, x2, f1, f2;
+			double d = 0.1 * E, n = 0;
+			if (!fi1.Exists)
+			{
+				//Create a file to write to.
+				using (StreamWriter sw = fi1.CreateText())
+				{
+					sw.WriteLine(" ");
+				}
+			}
+            *//*using (StreamReader sr = fi1.OpenText())
+			{
+				string s = "";
+				while ((s = sr.ReadLine()) != null)
+				{
+					Console.WriteLine(s);
+				}
+			}*//*
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine("n\t a\t b\t x1\t x2\t f(x1)\t f(x2)\n\n");
+            }
+			//std::cout << "n\t a\t b\t x1\t x2\t f(x1)\t f(x2)\n\n";
+			using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+			{
+				while (b - a > E)
+			{
+				
+					x2 = x1 = (a + b) / 2;
+					x1 -= d;
+					x2 += d;
+					f1 = f(x1);
+					f2 = f(x2);
+                    
+
+					//sw.WriteLine("n\t a\t b\t x1\t x2\t f(x1)\t f(x2)\n\n");
+					sw.WriteLine(n + "\t" + a + "\t" + b + "\t" + x1 + "\t" + x2 + "\t" + f1 + "\t" + f2 + "\n");
+
+
+					// Вывод промежуточных результатов
+					*//*std::cout << std::setprecision(5) << n << "\t";
+					std::cout << a << "\t" << b << "\t";
+					std::cout << x1 << "\t" << x2 << "\t";
+					std::cout << f1 << "\t" << f2 << "\n";*//*
+
+					n++;
+
+					//Если f1 меньше f2 то b = x2 иначе a = x1
+					//f1 < f2 ? b = x2 : a = x1;
+					if (f1 < f2)
+					{
+						b = x2;
+					}
+					else
+					{
+						a = x1;
+					}
+
+				}
+			}
+			Process.Start("notepad.exe", path);
+			return ((a + b) / 2);
+		}*/
+		public Form1()
+        {
+			Stopwatch stopWatch = new Stopwatch();
+			InitializeComponent();
+			double x;
+			double y;
+			double a = -100, b = 100, E = 0.001;
+			string path = Path.GetTempFileName();
+			FileInfo fi1 = new FileInfo(path);
+			double x1, x2, f1, f2;
+			double d = 0.1 * E, n = 0;
+			if (!fi1.Exists)
+			{
+				//Create a file to write to.
+				using (StreamWriter sw = fi1.CreateText())
+				{
+					sw.WriteLine(" ");
+				}
+			}
+			/*using (StreamReader sr = fi1.OpenText())
+			{
+				string s = "";
+				while ((s = sr.ReadLine()) != null)
+				{
+					Console.WriteLine(s);
+				}
+			}*/
+			
+				
+			
+		
+			for (int i = -10; i <= 5; i++)
+            {
+				chart1.Series[0].Points.AddXY(i, f(i));
+			}
+			//std::cout << "n\t a\t b\t x1\t x2\t f(x1)\t f(x2)\n\n";
+			using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+			{
+				sw.WriteLine("МЕТОД ДИХОТОМИИ");
+				sw.WriteLine("n\t a\t b\t x1\t x2\t f(x1)\t f(x2)\n\n");
+				stopWatch.Start();
+				while (b - a > E)
+				{
+
+					x2 = x1 = (a + b) / 2;
+					x1 -= d;
+					x2 += d;
+					f1 = f(x1);
+					f2 = f(x2);
+					
+
+					//sw.WriteLine("n\t a\t b\t x1\t x2\t f(x1)\t f(x2)\n\n");
+					sw.WriteLine(n + "\t" + a + "\t" + b + "\t" + x1 + "\t" + x2 + "\t" + f1 + "\t" + f2 + "\n");
+
+
+					// Вывод промежуточных результатов
+					/*std::cout << std::setprecision(5) << n << "\t";
+					std::cout << a << "\t" << b << "\t";
+					std::cout << x1 << "\t" << x2 << "\t";
+					std::cout << f1 << "\t" << f2 << "\n";*/
+
+					n++;
+
+					//Если f1 меньше f2 то b = x2 иначе a = x1
+					//f1 < f2 ? b = x2 : a = x1;
+					if (f1 < f2)
+					{
+						b = x2;
+					}
+					else
+					{
+						a = x1;
+					}
+					
+				}
+				stopWatch.Stop();
+				x = ((a + b) / 2);
+				y = f(x);
+				sw.WriteLine("X:= " + x + " " + "Y:= " + y);
+				TimeSpan ts = stopWatch.Elapsed;
+
+				// Format and display the TimeSpan value.
+				string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+					ts.Hours, ts.Minutes, ts.Seconds,
+					ts.Milliseconds / 10);
+				sw.WriteLine("Время работы алгоритма:=  "+ts);
+				sw.WriteLine("------------------------------------------------------------------------");
+				sw.WriteLine("МЕТОД ЗОЛОТОГО СЕЧЕНИЯ ");
+				//
+				Stopwatch stopWatch2 = new Stopwatch();
+				double a2 = -100, b2 = 100,  z = (3 - Math.Sqrt(5)) / 2;
+				double x3 = a2 + z * (b2 - a2), x4 = b2 - z * (b2 - a2);
+				stopWatch2.Start();
+				for (int i = 0; b2 - a2 > E; i++)
+				{
+					sw.WriteLine(i + "\t" + a2 + "\t" + b2 + "\t" + x3 + "\t" + x4 + "\t" + f(x3) + "\t" + f(x4) + "\n");
+					if (f(x3) <= f(x4))
+					{
+						b2 = x4;
+						x4 = x3;
+						x3 = a2 + b2 - x4;
+					}
+					else
+					{
+						a2 = x3;
+						x3 = x4;
+						x4 = a2 + b2 - x3;
+					}
+				}
+				stopWatch2.Stop();
+				TimeSpan ts2 = stopWatch2.Elapsed;
+				string elapsedTime2 = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+					ts.Hours, ts.Minutes, ts.Seconds,
+					ts.Milliseconds / 10);
+				double x5 = (a2 + b2) / 2;
+				sw.WriteLine("X:= " + x5 + " " + "Y:= " + f(x5));
+				sw.WriteLine("Время работы алгоритма:=  " + ts2);
+
+				//
+
+			}
+			Process.Start("notepad.exe", path);
+			//return ((a + b) / 2);
+			 x = ((a + b) / 2);
+			 y = f(x);
+
+			label3.Text = x.ToString();
+			label4.Text = y.ToString();
+			chart1.Series[1].Points.AddXY(x, y);
+			//chart1->Series[0]->Points->AddXY(j, FUNC(j));
+
+
+		}
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
